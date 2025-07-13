a2070decd7778440c281e040524967e873348728
Sirius083 <1518246621@qq.com>
2025-07-13 11:03:14 +0800

    update

--- tfapp_book.sh
+++ tfapp_book.sh
@@ -7,3 +7,21 @@ python train_insightface_book.py --RFW_race Caucasian --lr 0.1 --max_epoch 27  -
 python utils_insightface.py
 
 
+git remote add origin https://github.com/lizhenstat/IS-FACE-Book.git
+
+
+
+git remote set-url origin git@github.com:lizhenstat/IS-FACE-Book.git
+
+
+git remote set-url origin git@github.com:lizhenstat/IS-FACE-Book.git
+
+
+ssh-keygen -t rsa -b 4096 -C "jlz_stat@stu.xjtu.edu.cn"
+
+
+### 创建密钥对
+ssh-keygen # id_rsa是私钥，id_rsa.pub是公钥（需要配置到github账户）
+
+### 测试
+ssh -T git@github.com
